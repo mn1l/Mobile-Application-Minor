1c3d46184392703c2e421172a233be99dc9cf80b
Karscoding <128476549+mn1l@users.noreply.github.com>
2025-01-08 15:38:30 +0100

    Minigame Start

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class GameController : MonoBehaviour
+{
+    
+    public GameObject EnterDoorButton;
+
+    private bool hasKey = false;
+    private bool doorOpen = false;
+
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Key"))
+        {
+            hasKey = true;
+            Debug.Log ("key has been picked up");
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.collider.CompareTag("Door") && hasKey)
+        {
+            hasKey = false;
+            doorOpen = true;
+            Debug.Log("Door has been opened");
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("OpenedDoor") && doorOpen)
+        {
+            EnterDoorButton.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other != null)
+        {
+            EnterDoorButton.SetActive(false);            
+        }
+    }
+
+    public void JumpIn()
+    {
+        Application.Quit();
+        SceneManager.LoadScene(SceneData.homepage);
+    }
+}
--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,30 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private InputActionReference movement;
     [SerializeField] private float speed;
 
     private Rigidbody2D rb;
-
-    public GameObject JumpButtonUI;
-
-    private bool hasKey = false;
-    private bool doorOpen = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 moveDirection = movement.action.ReadValue<Vector2>(); // Read the movement input direction
@@ -32,43 +20,5 @@ public class PlayerController : MonoBehaviour
         //transform.Translate( Time.deltaTime * speed * moveDirection ); // Move the player
     }
     
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.collider.CompareTag("Key"))
-        {
-            hasKey = true;
-            Debug.Log ("key has been picked up");
-            other.gameObject.SetActive(false);
-        }
-
-        if (other.collider.CompareTag("Door") && hasKey)
-        {
-            hasKey = false;
-            doorOpen = true;
-            Debug.Log("Door has been opened");
-            other.gameObject.SetActive(false);
-        }
-    }
-
-    private void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.collider.CompareTag("OpenedDoor") && doorOpen)
-         {
-             JumpButtonUI.SetActive(true);
-         }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other != null)
-        {
-        JumpButtonUI.SetActive(false);            
-        }
-    }
-
-    public void JumpIn()
-    {
-        Application.Quit();
-        SceneManager.LoadScene(SceneData.homepage);
-    }
+    
 }
