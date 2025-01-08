4ca133ebc2fe87de88dd76438436b03d328ffa66
Karscoding <128476549+mn1l@users.noreply.github.com>
2025-01-08 14:22:07 +0100

    Fixed collision + Added 2D Dynamic camera + nice background
    Fixed collision by using rigidbody for movement

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -12,16 +12,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private InputActionReference movement;
     [SerializeField] private float speed;
 
+    private Rigidbody2D rb;
+
     public GameObject JumpButtonUI;
 
     private bool hasKey = false;
     private bool doorOpen = false;
-    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector2 moveDirection = movement.action.ReadValue<Vector2>(); // Read the movement input direction
-        transform.Translate( Time.deltaTime * speed * moveDirection ); // Move the player
+        rb.velocity = moveDirection * speed;
+        //transform.Translate( Time.deltaTime * speed * moveDirection ); // Move the player
     }
     
     private void OnCollisionEnter2D(Collision2D other)
