1fdc6b108bac8dba56c21e194b833814501d861b
mn1l <128476549+mn1l@users.noreply.github.com>
2025-01-22 21:06:38 +0100

    Fixed collision

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference movement;
     [SerializeField] private float speed;
+        
+    [Header("Buttons")]
+    public GameObject EnterDoorButton;
+    
+    [Header("Key Icons")]
+    public GameObject KeyIcon1;
+    public GameObject KeyIcon2;
+    public GameObject KeyIcon3;
+
+    private bool hasKeyOne = false;
+    private bool hasKeyTwo = false;
+    private bool hasKeyThree = false;
+    
+    private bool doorOpen = false;
 
     private Rigidbody2D rb;
 
@@ -20,8 +35,81 @@ public class PlayerController : MonoBehaviour
         //transform.Translate( Time.deltaTime * speed * moveDirection ); // Move the player
     }
     
-    /*
-     * TODO
-     * Create new movement with arrow buttons, design choice as it improves QOL when playing and moving through the maze
-     */
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.collider.tag)
+        {
+            case "KeyOne":
+                hasKeyOne = true;
+                KeyIcon1.GetComponent<UnityEngine.UI.Image>().color = new Color(1f, 1f, 1f, 1f);
+                other.gameObject.SetActive(false);
+                break;
+
+            case "KeyTwo":
+                hasKeyTwo = true;
+                KeyIcon2.GetComponent<UnityEngine.UI.Image>().color = new Color(1f, 1f, 1f, 1f);
+                other.gameObject.SetActive(false);
+                break;
+
+            case "KeyThree":
+                hasKeyThree = true;
+                KeyIcon3.GetComponent<UnityEngine.UI.Image>().color = new Color(1f, 1f, 1f, 1f);
+                other.gameObject.SetActive(false);
+                break;
+
+            case "Door":
+                if (hasKeyOne && hasKeyTwo && hasKeyThree)
+                {
+                    hasKeyOne = false;
+                    hasKeyTwo = false;
+                    hasKeyThree = false;
+                    doorOpen = true;
+                    other.gameObject.SetActive(false);
+                }
+                break;
+        }
+    }
+    
+    //----------
+    
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        switch (other.collider.tag)
+        {
+            case "KeyOne":
+                //
+            case "KeyTwo":
+                // do something
+            case "KeyThree":
+                // do something
+            case "OpenedDoor":
+                if (doorOpen)
+                {
+                    EnterDoorButton.SetActive(true);
+                }
+                break;
+        }
+    }
+    
+    //-------------
+    
+    // Checks whether collision is true, if not then door button isn't visible
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other != null)
+        {
+            EnterDoorButton.SetActive(false);            
+        }
+    }
+    
+    // Enter door and finish level
+    public void EnterDoor()
+    {
+        Application.Quit();
+        SceneManager.LoadScene(SceneData.homepage);
+        
+        // Unlock next level in level overview
+        // Jump to next level scene
+    }
 }
